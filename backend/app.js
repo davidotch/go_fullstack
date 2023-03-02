cae8dc52fa67a3e70b72eb7cc0e166ba4183734f
David Cardon <da.cardon@outlook.fr>
2023-03-02 10:51:03 +0100

    add app.put

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const Thing = require('./models/Thing');
+const mongoose = require("mongoose");
+const Thing = require("./models/Thing");
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 mongoose
    .connect(
       "mongodb+srv://DCardon:i9vuo9IE3V5w43Cp@cluster0.vf47xko.mongodb.net/?retryWrites=true&w=majority",
@@ -27,10 +27,16 @@ app.use((req, res, next) => {
    next();
 });
 
+app.put("/api/stuff/:id", (req, res, next) => {
+   Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: "Objet modifié !" }))
+      .catch((error) => res.status(400).json({ error }));
+});
+
 app.post("/api/stuff", (req, res, next) => {
    delete req.body._id;
    const thing = new Thing({
-      ...req.body
+      ...req.body,
    });
    thing
       .save()
@@ -39,16 +45,14 @@ app.post("/api/stuff", (req, res, next) => {
 });
 
 app.get("/api/stuff/:id", (req, res, next) => {
-   Thing
-      .findOne({ _id: req.params.id })
+   Thing.findOne({ _id: req.params.id })
       .then((thing) => res.status(200).json(thing))
       .catch((error) => res.status(404).json({ error }));
 });
 
 // Un middleware est un bloc de code qui traite les requêtes et réponses de votre application
 app.get("/api/stuff", (req, res, next) => {
-   Thing
-      .find()
+   Thing.find()
       .then((things) => res.status(200).json(things))
       .catch((error) => res.status(400).json({ error }));
 });
