316ae1ad49406fce757fc4f50ff908643a5c103f
David Cardon <da.cardon@outlook.fr>
2023-03-03 09:46:57 +0100

    fix indentation

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const stuffControllers = require("../controllers/stuff");
 
+router.post("/", stuffControllers.createThing);
 router.put("/:id", stuffControllers.modifyThing);
 router.delete("/:id", stuffControllers.deleteThing);
-router.post("/", stuffControllers.createThing);
 router.get("/:id", stuffControllers.getOneThing);
 router.get("/", stuffControllers.getAllThing);
 
